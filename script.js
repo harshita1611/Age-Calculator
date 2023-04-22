c9ad2518076fd81a1f5376e83a9d6bd068366eb6
Harshita Bhambhani <97826462+harshita1611@users.noreply.github.com>
2023-04-23 00:57:22 +0530

    finally chalega

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const button = document.getElementById('button');
 const input = document.getElementById('input');
 
 const fetchdate = (input)=>{
-  fetch("https://sourdualmemorypool.devvrat-singhsi.repl.co/?date="+ input)
+  fetch("https://age-calculator.harshitabhambha.repl.co/?date="+ input)
   .then((res) => res.json())
   .then((res) => output.innerText =res["output"]);
 }
